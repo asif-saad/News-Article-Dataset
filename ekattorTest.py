0d36e4a2768d106cdede9501a4fa325a87eb8aed
Prussian Blue <asifsaad730@gmail.com>
2023-09-29 17:47:10 +0600

    working

--- ekattorTest.py
+++ ekattorTest.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url='https://ekattor.tv/politics/'
+url='https://ekattor.tv/sports/35865/%E0%A6%95%E0%A6%BE%E0%A6%A4%E0%A6%BE%E0%A6%B0%E0%A7%87-%E0%A6%AA%E0%A7%8B%E0%A6%B6%E0%A6%BE%E0%A6%95%E0%A7%87%E0%A6%B0-%E0%A6%95%E0%A6%BE%E0%A6%B0%E0%A6%A3%E0%A7%87-%E0%A6%86%E0%A6%B2%E0%A7%8B%E0%A6%9A%E0%A6%BF%E0%A6%A4-%E0%A6%95%E0%A7%8D%E0%A6%B0%E0%A7%8B%E0%A7%9F%E0%A6%BE%E0%A6%9F-%E0%A6%AE%E0%A6%A1%E0%A7%87%E0%A6%B2'
 cnt=0
 final=[url]
 
@@ -28,15 +28,13 @@ while cnt<800:
 
         
 
-        with open('output.txt', 'a', encoding='utf-8') as file:
-            title=soup.find('div',class_='tag_title_holder')
-            title=title.find('h1').text
-            file.write(str(cnt)+'\t'+title)
+        with open('output1.txt', 'a', encoding='utf-8') as file:
 
-        # div=soup.find_all('div',class_='tab_warp')
-        # for x in div:
-        #     x=x.find('a')
-        #     print(x.get('href'))
+            title=soup.find('h1',class_='title')
+            if title:
+
+                file.write(title.text+'\n\n')
+
                     
 
 
@@ -44,6 +42,10 @@ while cnt<800:
         print('Failed to retrieve the web page. Status code:', response.status_code)
     
     cnt+=1
-    print(cnt,len(final))
+    if cnt<len(final):
+        url=final[cnt]
+    else:
+        break
+    print(cnt,len(final),round(cnt/len(final),3))
 
 
