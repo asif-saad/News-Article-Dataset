3073aec8d3c244bdf57bee555967b0ac4ab0f042
Prussian Blue <asifsaad730@gmail.com>
2023-10-05 15:08:15 +0600

    ok

--- ittefaq.py
+++ ittefaq.py
@@ -1,10 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+from datasets.dataset_dict import DatasetDict
+from datasets import Dataset
+import jsonlines
 
+titleFinal=str()
+categoryFinal=str()
+timeFinal=str()
+contentFinal=str()
+tagsFinal=str()
+d={'EkattorTv':Dataset.from_dict({'Title':titleFinal,'Category':categoryFinal,'Time':timeFinal,'Content':contentFinal,'Tags':tagsFinal})}
+raw_datasets=DatasetDict(d)
 
 url1='https://www.ittefaq.com.bd/'
-cnt=15817
+cnt=16346
+
+
 
 
 while True:
@@ -18,58 +29,99 @@ while True:
         category=soup.find('h2',class_='secondary_logo')
         date=soup.find('div',class_='each_row time')
         tags=soup.find('div',class_='topic_list')
+
+
+
         if h1 and contents:
-            # titles
-            h1=h1.text
-
-
-
-
-            # contents
-            contents=contents.find_all('span')
-            content=str()
-            for x in contents:
-                content+=x.text
-            # print(content)
-
-
-
-            # category
-            category=category.find('span')
-            # print(category.text)
+            with open('output.txt','a',encoding='utf-8') as file:
+                with jsonlines.open("C:/Users/asifs/OneDrive/Desktop/dataset/ittefaq.jsonl", "a") as writer:
+                    titleFinal=str()
+                    categoryFinal=str()
+                    timeFinal=str()
+                    contentFinal=str()
+                    tagsFinal=str()
 
 
 
 
 
-            # time
-            date=date.find('span',class_='tts_time')
-            # print(date.text)
+                    # titles
+                    h1=h1.text
+                    titleFinal=h1
+                    file.write(str(cnt-15817)+','+str(cnt)+'\n'+titleFinal+'\n')
+
+
+
+
+                    # category
+                    if category:
+                        category=category.find('span')
+                        # print(category.text)
+                        if category:
+                            categoryFinal=category.text
+                            file.write(categoryFinal+'\n')
+
+
+
+                    # time
+                    if date:
+                        date=date.find('span',class_='tts_time')
+                        # print(date.text)
+                        if date:
+                            timeFinal=date.text
+                            file.write(timeFinal +'\n')
 
 
 
 
 
-            # tags
-            tags=tags.find_all('strong')
-            tag=str()
-            for x in tags:
-                tag+=x.text
-                if x!=tag[-1]:
-                    tag+=', '
-            
-            # print(tag)
+                    # contents
+                    contents=contents.find_all('span')
+                    if contents:
+                        content=str()
+                        for x in contents:
+                            content+=x.text
+                        # print(content)
+                        contentFinal=content
+                        file.write(contentFinal+'\n')
 
+
+
+
+
+                    # tags
+                    if tags:
+                        tags=tags.find_all('strong')
+                        if tags:
+                            tag=str()
+                            for x in tags:
+                                tag+=x.text
+                                if x!=tag[-1]:
+                                    tag+=', '
+                            
+                            # print(tag)
+                            tagsFinal=tag
+                            file.write('tags:'+tagsFinal)
+
+
+                    file.write('\n\n\n')
+                    writer.write({'Title':titleFinal,'Category':categoryFinal,'Time':timeFinal,
+                                  'Content':contentFinal,'Tags':tagsFinal})
+
+                            
 
 
     else:
         print('Failed to retrieve the web page. Status code:', response.status_code)
         
     
+
+    if cnt%250==0:
+        import time
+        time.sleep(50)
+
     cnt+=1
-    break
-    if cnt%2000==0:
-        time.sleep(100)
+    
     
 
 
