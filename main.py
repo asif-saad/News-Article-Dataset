18e93926a0d6bfd2c9302e7026d733bfd000fe38
Prussian Blue <asifsaad730@gmail.com>
2023-10-16 14:29:38 +0600

    355k

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-file_path='C:/Users/asifs/OneDrive/Desktop/News-Article-Dataset/test.py'
+import os
+file_path='C:/Users/asifs/OneDrive/Desktop/News-Article-Dataset/ittefaqMain.py'
 while True:
-    with open(file_path,'r') as file:
-        python_code=file.read()
-        exec(python_code)
-        print("ok, done")
+    os.system(f'python {file_path}')
+    print("ok, done ok, done ok, doneok, done ok, doneok, doneok, doneok, doneok, doneok, donevok, doneok, doneok, doneok, doneok, done")
+    # with open(file_path,'r') as file:
+    #     python_code=file.read()
+    #     exec(python_code)
+    #     print("ok, done")
